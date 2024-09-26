8d1fbe7389272cfe6db50610fe270a0674032e46
Andrew Straw <strawman@astraw.com>
2024-09-26 12:41:53 +0200

    firmware for beamdriver
    This is essentially commit
    f09567209f743149737f25a288ec7477a84578f4 from the
    old repository.

---
+++ firmware/beamdriver/src/main.rs
@@ -0,0 +1,183 @@
+#![no_std]
+#![no_main]
+
+// Pins in use:
+//  gp6: PWM for pan servo
+//  gp7: PWM for tilt servo
+//  gp8: connected to led driver, high = beam on
+//  gp18: cam0 line1 (not used but connected)
+//  gp19: cam1 line1 (not used but connected)
+//  gp20: cam0 line2, triggers the cam
+//  gp21: cam1 line2, triggers the cam
+//  gp22: diode-OR'ed line3 from cam0 and cam1, should be configured as ExposureActive output (active high) on the cams.
+
+// Reserved Pins (do not use except for these assignments):
+//  Pico (but not Pico W) LED: gpio25
+//  I2C0 SDA: gpio4
+//  I2C0 SCL: gpio5
+//  Pico W wireless: gpio23
+//  Pico W wireless: gpio24
+//  Pico W wireless: gpio25
+//  Pico W wireless: gpio29
+
+use defmt_rtt as _;
+use panic_reset as _; //it is important to reset asap so that the beam driver/led don't burn out
+
+use embedded_hal::{
+    digital::v2::InputPin,
+    digital::v2::OutputPin,
+    timer::{Cancel, CountDown},
+    PwmPin,
+};
+use hal::multicore::{Multicore, Stack};
+use hal::pac;
+use rp2040_hal::{self as hal, pwm::Slices as PWMSlices, Sio};
+use rp_pico::XOSC_CRYSTAL_FREQ;
+
+use fugit::MicrosDurationU32;
+
+const MAX_BEAM_DURATION_US: u32 = 500;
+const FRAMERATE: f64 = 150.0;
+const FRAMERATE_CLOCK_DIVIDER: u8 = 20; //divider should be high enough to allow 16-bit counting. If 125.0e6/(divider * framerate) is < 65536 then we're good.
+
+// core0: usb, servo control
+// core1: fully dedicated to beam drive. With exposure times in 50-microsecond zone, it
+//   is critical that the latency is very small. Interrupts don't seem to be fast enough.
+
+/// Stack for core 1
+///
+/// Core 0 gets its stack via the normal route - any memory not used by static values is
+/// reserved for stack and initialised by cortex-m-rt.
+/// To get the same for Core 1, we would need to compile everything seperately and
+/// modify the linker file for both programs, and that's quite annoying.
+/// So instead, core1.spawn takes a [usize] which gets used for the stack.
+/// NOTE: We use the `Stack` struct here to ensure that it has 32-byte alignment, which allows
+/// the stack guard to take up the least amount of usable RAM.
+static mut CORE1_STACK: Stack<4096> = Stack::new();
+
+fn core1_task() -> ! {
+    let mut pac = unsafe { pac::Peripherals::steal() };
+    let _core = unsafe { pac::CorePeripherals::steal() };
+
+    let sio = Sio::new(pac.SIO);
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut beam = pins.gpio8.into_push_pull_output();
+    beam.set_low().unwrap();
+
+    let exposure_active = pins.gpio22.into_pull_down_input();
+
+    //set up trigger generator
+    let pwm_slices = PWMSlices::new(pac.PWM, &mut pac.RESETS);
+    let mut trigpwm = pwm_slices.pwm2;
+    let divider = FRAMERATE_CLOCK_DIVIDER; //divider should be high enough to not let mytop exceed 65535
+    let freq = FRAMERATE; //flicker frequency in hz
+    let mytop = (125.0e6 / (divider as f64) / freq) as u16 - 1;
+    let myduty = mytop / 2;
+    trigpwm.default_config();
+    trigpwm.set_div_int(divider);
+    trigpwm.set_top(mytop);
+    trigpwm.channel_a.set_duty(myduty);
+    trigpwm.channel_a.output_to(pins.gpio20);
+    trigpwm.channel_b.set_duty(myduty);
+    trigpwm.channel_b.output_to(pins.gpio21);
+    trigpwm.enable();
+
+    //beam duration limiter timer
+    let tt = hal::Timer::new(pac.TIMER, &mut pac.RESETS);
+    let mut timer = tt.count_down();
+
+    //state machine
+    enum MyState {
+        Idle,     // exposure low, beam off
+        Exposure, // exposure high, beam on
+        Limit,    // exposure high and has been high for too long, beam off
+    }
+    let mut state = MyState::Idle;
+
+    loop {
+        if exposure_active.is_high().unwrap() {
+            match state {
+                MyState::Idle => {
+                    beam.set_high().unwrap();
+                    state = MyState::Exposure;
+                    timer.start(MicrosDurationU32::micros(MAX_BEAM_DURATION_US));
+                }
+                MyState::Exposure => {
+                    match timer.wait() {
+                        Ok(()) => {
+                            //timeout. Abort exposure
+                            beam.set_low().unwrap();
+                            //timer.cancel();
+                            state = MyState::Limit;
+                        }
+                        _ => {}
+                    }
+                }
+                MyState::Limit => {
+                    //do nothing, wait for exposure_active to go low
+                }
+            }
+        } else {
+            match state {
+                MyState::Idle => {
+                    //do nothing, remain idle
+                }
+                MyState::Exposure => {
+                    //exposure ended without reaching the limit.
+                    beam.set_low().unwrap();
+                    timer.cancel().unwrap();
+                    state = MyState::Idle;
+                }
+                MyState::Limit => {
+                    //exposure ended, but was too long.
+                    state = MyState::Idle;
+                }
+            }
+        }
+    }
+}
+
+// core0
+#[rp2040_hal::entry]
+fn main() -> ! {
+    let mut pac = pac::Peripherals::take().unwrap();
+    let _core = pac::CorePeripherals::take().unwrap();
+
+    let mut watchdog = hal::watchdog::Watchdog::new(pac.WATCHDOG);
+
+    let _clocks = hal::clocks::init_clocks_and_plls(
+        XOSC_CRYSTAL_FREQ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut sio = Sio::new(pac.SIO);
+    let _pins = rp_pico::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    //start core1 task
+    let mut mc = Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio.fifo);
+    let cores = mc.cores();
+    let core1 = &mut cores[1];
+    let _test = core1.spawn(unsafe { &mut CORE1_STACK.mem }, move || core1_task());
+
+    loop {
+        cortex_m::asm::wfi();
+    }
+}
