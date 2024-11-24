172b859df17c9c0462fea10a6f5a3df3683afe38
Andrew Straw <strawman@astraw.com>
2024-11-24 07:32:06 +0100

    flo: git hash is part of version

--- build.rs
+++ build.rs
@@ -1,17 +1,18 @@
-use std::process::Command;
-
-fn git_hash() -> String {
-    let output = Command::new("git")
+/// Set the environment variables `GIT_HASH` AND `CARGO_PKG_VERSION` to include
+/// the current git revision.
+fn git_hash() -> Result<(), Box<(dyn std::error::Error)>> {
+    let output = std::process::Command::new("git")
         .args(["rev-parse", "HEAD"])
-        .output()
-        .expect("git");
-    String::from_utf8(output.stdout)
-        .expect("from_utf8")
-        .trim()
-        .to_string()
+        .output()?;
+    let git_hash = String::from_utf8(output.stdout)?;
+    println!("cargo:rustc-env=GIT_HASH={git_hash}");
+    let orig_version = env!("CARGO_PKG_VERSION");
+    let version = format!("{orig_version}+{git_hash}");
+    println!("cargo:rustc-env=CARGO_PKG_VERSION={version}"); // override default
+    Ok(())
 }
 
-fn main() {
-    let git_rev = git_hash();
-    println!("cargo:rustc-env=GIT_HASH={}", git_rev);
+fn main() -> Result<(), Box<(dyn std::error::Error)>> {
+    git_hash()?;
+    Ok(())
 }
