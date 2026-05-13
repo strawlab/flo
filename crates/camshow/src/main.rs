608314e96debee67278fad8fd751dacad75222e8
Andrew Straw <strawman@astraw.com>
2026-05-13 12:55:19 +0200

    camshow: save to logfile

--- crates/camshow/src/main.rs
+++ crates/camshow/src/main.rs
@@ -46,13 +46,35 @@ struct Cli {
     /// pipeline without the rest of the stack running.
     #[arg(long)]
     test_pattern: bool,
+
+    /// Directory for log files. Defaults to the home directory.
+    #[arg(long)]
+    log_dir: Option<camino::Utf8PathBuf>,
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    init_tracing()?;
+
+    if std::env::var_os("RUST_LOG").is_none() {
+        // SAFETY: set in single-threaded code at startup, before any other
+        // thread that might read RUST_LOG has been spawned.
+        unsafe { std::env::set_var("RUST_LOG", "camshow=info,info") };
+    }
 
     let cli = Cli::parse();
+
+    let log_dir = if let Some(d) = cli.log_dir.as_ref() {
+        d.clone()
+    } else {
+        let home =
+            home::home_dir().ok_or_else(|| eyre::eyre!("cannot determine home directory"))?;
+        camino::Utf8PathBuf::from_path_buf(home)
+            .map_err(|p| eyre::eyre!("home directory is not valid UTF-8: {}", p.display()))?
+    };
+    std::fs::create_dir_all(&log_dir)
+        .with_context(|| format!("creating log directory {log_dir}"))?;
+
+    init_tracing(&log_dir)?;
 
     let (display_tx, display_rx) = watch::channel::<Option<DisplayFrame>>(None);
     let (osd_tx, osd_rx) = watch::channel::<Option<OsdSnapshot>>(None);
@@ -126,34 +148,47 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn init_tracing() -> Result<()> {
-    if std::env::var_os("RUST_LOG").is_none() {
-        // SAFETY: set in single-threaded code at startup, before any other
-        // thread that might read RUST_LOG has been spawned.
-        unsafe { std::env::set_var("RUST_LOG", "camshow=info,info") };
-    }
+fn init_tracing(log_dir: &camino::Utf8Path) -> Result<()> {
+    use std::sync::Mutex;
     use time::{UtcOffset, format_description::well_known::Iso8601};
     use tracing_subscriber::{
         fmt::{self, time::OffsetTime},
         layer::SubscriberExt,
     };
 
+    let log_file_name = chrono::Local::now()
+        .format(".camshow-%Y%m%d_%H%M%S.%f.log")
+        .to_string();
+    let full_log_path = log_dir.join(&log_file_name);
+
     let timer = OffsetTime::new(
         UtcOffset::from_whole_seconds(chrono::Local::now().offset().local_minus_utc())?,
         Iso8601::DEFAULT,
     );
+
     #[cfg(target_os = "windows")]
     ansi_term::enable_ansi_support()
         .map_err(|code| eyre::eyre!("Failed setting windows ansi: {code}"))?;
 
+    let file = std::fs::File::create(&full_log_path)
+        .with_context(|| format!("creating log file {full_log_path}"))?;
+    let file_writer = Mutex::new(file);
+    let file_layer = fmt::layer()
+        .with_timer(timer.clone())
+        .with_writer(file_writer)
+        .with_ansi(false)
+        .with_file(true)
+        .with_line_number(true);
     let console_layer = fmt::layer()
         .with_timer(timer)
         .with_file(true)
         .with_line_number(true);
     let collector = tracing_subscriber::registry()
+        .with(file_layer)
         .with(console_layer)
         .with(tracing_subscriber::filter::EnvFilter::from_default_env());
     tracing::subscriber::set_global_default(collector)?;
     std::panic::set_hook(Box::new(tracing_panic::panic_hook));
+    tracing::info!("logging to {full_log_path}");
     Ok(())
 }
